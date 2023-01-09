fd7baa7f1b055187dcf966b98745d7cbb2295a19
Showri <Showri.srinivas@ionixxtech.com>
2023-01-09 19:28:21 +0530

    Package updates

--- app/src/lib/assets/index.ts
+++ app/src/lib/assets/index.ts
@@ -1,5 +1,5 @@
 export const Icons = {
-  logo: "/src/lib/icon/logo.svg",
-  rightArrow: "/src/lib/icon/logo.svg",
-  background: "/src/lib/icon/logo.svg",
+  logo:require("/src/lib/assets/logo.svg"),
+  rightArrow: require("/src/lib/assets/arrow-right.svg"),
+  background: require("/src/lib/assets/background.svg"),
 };
