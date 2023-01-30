5d73f4ceeb3f927462a5d5d0858d99516bab30e2
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-01-30 19:46:39 +0530

    fixes

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -2,15 +2,14 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
     stories: [
-        '../src/**/*.stories.mdx',
-        '../src/**/*.stories.@(js|jsx|ts|tsx)',
-        '../src/**/**/*.stories.@(js|jsx|ts|tsx)',
+        '../src/stories/*.stories.mdx',
+        '../src/stories/*.stories.@(js|jsx|ts|tsx)',
+        '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
     ],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        'storybook-addon-styled-component-theme/dist/preset',
     ],
     framework: '@storybook/react',
     core: {
--- app/src/stories/delivery.stories.tsx
+++ app/src/stories/delivery.stories.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import MojitoCheckout from '../lib/public/MojitoCheckout';
+import Header from '@lib/components/shared/Header';
 
-const stories = storiesOf('Payment', module);
-
-
-
-stories.add('Hide Express checkout', () => {
-  return (
-    <MojitoCheckout
-      show
-      uiConfiguration={{
-        billing: {
-          hideExpressCheckout: true,
-        },
-      }}
-      deliveryConfiguration={{
-        orgId: 'd086ea16-d40d-454c-84a4-64b5e940670a',
-        lotId: '17cd1000-323d-4a20-8e5f-7a8598ffae2a',
-        itemCount: 1,
-      }} />
-  );
-});
-
-
+export const HeaderStories = () => {
+    return <Header />;
+};
+const stories = {
+    title: 'components/Header',
+    component: Header,
+};
 export default stories;
