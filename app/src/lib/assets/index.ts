4147eef2cc14b0df30dddbf831fefd203ebd7f03
Showri <Showri.srinivas@ionixxtech.com>
2023-02-08 01:06:05 +0530

    Update path

--- app/src/lib/assets/index.ts
+++ app/src/lib/assets/index.ts
@@ -1,22 +1,22 @@
 const logo = require('./logo.svg');
+const rightArrow =  require('./arrow-right.svg');
+const background =  require('./background.svg');
+const item =  require('./item.svg');
+const gpay =  require('./gpay.svg');
+const applepay =  require('./applepay.svg');
+const metamask =  require('./metamask.svg');
+const walletConnect =  require('./wallet-connect.svg');
+const wireTransfer =  require('./wiretransfer.svg');
+const lock =  require('./lock.svg');
+const creditCards =  require('./creditcards.svg');
+const gpayDark =  require('./gpay-dark.svg');
+const applepayDark =  require('./applepay-dark.svg');
+const visaCard =  require('./visa.svg');
+const masterCard =  require('./masterCard.svg');
+const americanExpress =  require('./americanExpress.svg');
+const loading =  require('./loading.svg');
+const walletAddress =  require('./walletAddress.svg');
 
-import rightArrow from './arrow-right.svg';
-import background from './background.svg';
-import item from './item.svg';
-import gpay from './gpay.svg';
-import applepay from './applepay.svg';
-import metamask from './metamask.svg';
-import walletConnect from './wallet-connect.svg';
-import wireTransfer from './wiretransfer.svg';
-import lock from './lock.svg';
-import creditCards from './creditcards.svg';
-import gpayDark from './gpay-dark.svg';
-import applepayDark from './applepay-dark.svg';
-import visaCard from './visa.svg';
-import masterCard from './masterCard.svg';
-import americanExpress from './americanExpress.svg';
-import loading from './loading.svg';
-import walletAddress from './walletAddress.svg';
 
 export const DEFAULT_ERROR_IMAGE_SRC = 'https://raw.githubusercontent.com/mojitoinc/mojito-mixers/main/app/src/lib/assets/mojito-error-loader.gif';
 
