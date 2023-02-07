609b915f8a1fbb0496b39fab5c358bef3777a52c
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-02-08 00:11:41 +0530

    Mojit

--- app/src/lib/public/MojitoCheckout.tsx
+++ app/src/lib/public/MojitoCheckout.tsx
@@ -5,7 +5,7 @@ import {
   ConfigurationType,
   DefaultConfiguration,
   makeUIConfiguration,
-} from '@providers/ConfigurationProvider';
+} from '@lib/providers/ConfigurationProvider';
 import { makeTheme, styles } from '@lib/theme';
 import MojitoCheckoutLayout from '@views/index';
 import { ThemeConfiguration } from '@lib/interfaces';
