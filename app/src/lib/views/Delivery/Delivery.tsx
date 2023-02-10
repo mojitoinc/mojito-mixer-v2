fd9cef3fcb65b115e6c42b87b5f81dfe2112792a
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-02-10 17:48:24 +0530

    lint fixes

--- app/src/lib/views/Delivery/Delivery.tsx
+++ app/src/lib/views/Delivery/Delivery.tsx
@@ -7,6 +7,7 @@ import {
   Stack,
   FormHelperText,
 } from '@mui/material';
+import Modal from 'react-modal';
 import { MixTheme } from '../../theme';
 import {
   Button,
@@ -20,7 +21,6 @@ import { ConnectType } from '../../providers/ConnectContext';
 import { Icons } from '../../assets';
 import { DeliveryInfoCard } from './DeliveryInfoCard';
 import { NEW_MULTI_SIG } from './index';
-import Modal from 'react-modal';
 import LoadingContainer from '../Loading';
 
 interface DeliveryProps {
@@ -50,7 +50,7 @@ const Delivery = ({
   connect,
   onDisconnect,
   error,
-  isLoading
+  isLoading,
 }: DeliveryProps) => {
   const theme = useTheme<MixTheme>();
   const { delivery } = useUIConfiguration();
@@ -169,20 +169,20 @@ const Delivery = ({
           }} />
       </Box>
 
-    <Modal
-      ariaHideApp={ false }
-      isOpen={ isLoading }
-      style={{
-        content: {
-          width: '100vw',
-          height: '100vh',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          padding: 0,
-        },
-      }}>
+      <Modal
+        ariaHideApp={ false }
+        isOpen={ isLoading }
+        style={{
+          content: {
+            width: '100vw',
+            height: '100vh',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            padding: 0,
+          },
+        }}>
         <LoadingContainer />
       </Modal>
     </>
