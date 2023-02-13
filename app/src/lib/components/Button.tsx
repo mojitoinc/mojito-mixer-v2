1f80f84ca5dd9c829181657d0a88bea1e57c0078
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-02-13 19:59:40 +0530

    lint fixes

--- app/src/lib/components/Button.tsx
+++ app/src/lib/components/Button.tsx
@@ -2,58 +2,56 @@ import { Button as MuiButton, SxProps, Theme, useTheme } from '@mui/material';
 import React from 'react';
 import { MixTheme } from '../theme';
 
-// interface ButtonProps {
-//   title?: string;
-//   onClick?: () => void;
-//   backgroundColor?: string;
-//   textColor?: string;
-//   sx?: SxProps<Theme>;
-//   children?: JSX.Element;
-//   variant?: 'text' | 'outlined' | 'contained';
-//   disabled?: boolean
-// }
-
-// const Button = ({
-//   title,
-//   onClick,
-//   backgroundColor,
-//   textColor,
-//   sx,
-//   children,
-//   variant,
-//   disabled,
-// }: ButtonProps) => {
-//   const theme = useTheme<MixTheme>();
-
-//   return (
-//     <MuiButton
-//       title={ title }
-//       autoCapitalize="none"
-//       size="small"
-//       disabled={ disabled }
-//       variant={ variant }
-//       sx={{
-//         backgroundColor: disabled ? theme.palette?.secondary?.main : backgroundColor ?? theme.palette?.primary?.main,
-//         color: textColor ?? theme.palette?.secondary?.main,
-//         textTransform: 'none',
-//         fontWeight: '700',
-//         fontSize: '16px',
-//         borderRadius: '4px',
-//         padding: '0px 22px',
-//         height: '40px',
-//         display: 'flex',
-//         flexDirection: 'row',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         ...sx,
-//       }}
-//       onClick={ onClick }>
-//       { children }
-//       { title }
-//     </MuiButton>
-//   );
-// };
-const Button = () => {
-  return <div></div>
+interface ButtonProps {
+  title?: string;
+  onClick?: () => void;
+  backgroundColor?: string;
+  textColor?: string;
+  sx?: SxProps<Theme>;
+  children?: JSX.Element;
+  variant?: 'text' | 'outlined' | 'contained';
+  disabled?: boolean
 }
+
+const Button = ({
+  title,
+  onClick,
+  backgroundColor,
+  textColor,
+  sx,
+  children,
+  variant,
+  disabled,
+}: ButtonProps) => {
+  const theme = useTheme<MixTheme>();
+
+  return (
+    <MuiButton
+      title={ title }
+      autoCapitalize="none"
+      size="small"
+      disabled={ disabled }
+      variant={ variant }
+      sx={{
+        backgroundColor: disabled ? theme.palette?.secondary?.main : backgroundColor ?? theme.palette?.primary?.main,
+        color: textColor ?? theme.palette?.secondary?.main,
+        textTransform: 'none',
+        fontWeight: '700',
+        fontSize: '16px',
+        borderRadius: '4px',
+        padding: '0px 22px',
+        height: '40px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...sx,
+      }}
+      onClick={ onClick }>
+      { children }
+      { title }
+    </MuiButton>
+  );
+};
+
 export default Button;
--- app/test-utils.tsx
+++ app/test-utils.tsx
@@ -1,6 +1,6 @@
-import { ReactTestRenderer, create, TestRendererOptions } from 'react-test-renderer'
-import React, { FC, ReactElement } from 'react'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ReactTestRenderer, create, TestRendererOptions } from 'react-test-renderer';
+import React, { ReactElement } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 const theme = createTheme({
   palette: {
@@ -10,11 +10,11 @@ const theme = createTheme({
       light: '01fff0',
     },
   },
-})
+});
 
 const RootProviders = (children: ReactElement, options?: TestRendererOptions): ReactTestRenderer => {
-  return create(<ThemeProvider theme={theme}>{children}</ThemeProvider>, options)
-}
+  return create(<ThemeProvider theme={ theme }>{ children }</ThemeProvider>, options);
+};
 
-export * from '@testing-library/react'
-export { RootProviders as render }
+export * from '@testing-library/react';
+export { RootProviders as render };
