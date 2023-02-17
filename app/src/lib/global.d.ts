1cb1a7ec072e88627c74bd475723e47500a248bb
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-02-17 13:49:57 +0530

    svg node global

--- app/src/lib/global.d.ts
+++ app/src/lib/global.d.ts
@@ -3,3 +3,7 @@ declare namespace NodeJS {
     browser: boolean;
   }
 }
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
