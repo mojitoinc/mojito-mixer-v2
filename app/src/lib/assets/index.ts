493bd4a7c7797a14c7e192cde1321667c3a45898
Alex INX <alexappadurai.swamydhas@ionixxtech.com>
2023-02-17 13:46:44 +0530

    Update index.ts

--- app/src/lib/assets/index.ts
+++ app/src/lib/assets/index.ts
@@ -1,24 +1,21 @@
-const BASE_URL = 'https://raw.githubusercontent.com/mojitoinc/mojito-mixer-v2/develop/app/src/lib/assets/';
-
-const logo = `${ BASE_URL }logo.svg`;
-const rightArrow = `${ BASE_URL }arrow-right.svg`;
-const background = `${ BASE_URL }background.svg`;
-const item = `${ BASE_URL }item.svg`;
-const gpay = `${ BASE_URL }gpay.svg`;
-const applepay = `${ BASE_URL }applepay.svg`;
-const metamask = `${ BASE_URL }metamask.svg`;
-const walletConnect = `${ BASE_URL }wallet-connect.svg`;
-const wireTransfer = `${ BASE_URL }wiretransfer.svg`;
-const lock = `${ BASE_URL }lock.svg`;
-const creditCards = `${ BASE_URL }creditcards.svg`;
-const gpayDark = `${ BASE_URL }gpay-dark.svg`;
-const applepayDark = `${ BASE_URL }applepay-dark.svg`;
-const visaCard = `${ BASE_URL }visa.svg`;
-const masterCard = `${ BASE_URL }masterCard.svg`;
-const americanExpress = `${ BASE_URL }americanExpress.svg`;
-const loading = `${ BASE_URL }loading.svg`;
-const walletAddress = `${ BASE_URL }walletAddress.svg`;
-
+import logo from './logo.svg';
+import rightArrow from './arrow-right.svg';
+import background from './background.svg';
+import item from './item.svg';
+import gpay from './gpay.svg';
+import applepay from './applepay.svg';
+import metamask from './metamask.svg';
+import walletConnect from './wallet-connect.svg';
+import wireTransfer from './wiretransfer.svg';
+import lock from './lock.svg';
+import creditCards from './creditcards.svg';
+import gpayDark from './gpay-dark.svg';
+import applepayDark from './applepay-dark.svg';
+import visaCard from './visa.svg';
+import masterCard from './masterCard.svg';
+import americanExpress from './americanExpress.svg';
+import loading from './loading.svg';
+import walletAddress from './walletAddress.svg';
 
 export const DEFAULT_ERROR_IMAGE_SRC = 'https://raw.githubusercontent.com/mojitoinc/mojito-mixers/main/app/src/lib/assets/mojito-error-loader.gif';
 
